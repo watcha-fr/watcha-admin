eb78dc811f00e780a5aeeacea4b40156f5cc30f2
Morisse <wigginsenders@gmail.com>
2018-08-30 14:18:38 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Bootstrap, Grid, Row, Col, Button,FormGroup,ControlLabel,FormControl,HelpBlock} from 'react-bootstrap';
+import {Bootstrap,Button,FormGroup,ControlLabel,FormControl,HelpBlock,Col,Checkbox,Form} from 'react-bootstrap';
 
 
 
@@ -12,16 +12,13 @@ class App extends Component {
   this.handleChange = this.handleChange.bind(this);
 
   this.state = {
-    value: ''
+    userName:'',
+    password: ''
   };
 }
 
-  getValidationState() {
-  const length = this.state.value.length;
-  if (length > 10) return 'success';
-  else if (length > 5) return 'warning';
-  else if (length > 0) return 'error';
-  return null;
+  onConnection() {
+
 }
 
 handleChange(e) {
@@ -29,23 +26,30 @@ handleChange(e) {
 }
   render() {
     return (
-      <form>
-             <FormGroup
-               controlId="formBasicText"
-               validationState={this.getValidationState()}
-             >
-               <ControlLabel>Working example with validation</ControlLabel>
-               <FormControl
-                 type="text"
-                 value={this.state.value}
-                 placeholder="Enter text"
-                 onChange={this.handleChange}
-               />
-               <FormControl.Feedback />
-               <HelpBlock>Validation is based on string length.</HelpBlock>
-             </FormGroup>
-           </form>
+      <Form horizontal>
+    <FormGroup controlId="formHorizontalName">
+      <Col componentClass={ControlLabel} sm={2}>
+        Name
+      </Col>
+      <Col sm={10}>
+        <FormControl type="text" placeholder="Name" onChange={evt => this.setState({userName: evt.target.value})}/>
+      </Col>
+    </FormGroup>
 
+    <FormGroup controlId="formHorizontalPassword">
+      <Col componentClass={ControlLabel} sm={2}>
+        Password
+      </Col>
+      <Col sm={10}>
+        <FormControl type="password" placeholder="Password" onChange={evt => this.setState({userName: evt.target.value})}/>
+      </Col>
+    </FormGroup>
+    <FormGroup>
+      <Col smOffset={2} sm={10}>
+        <Button type="submit" onClick={(event) => this.onConnection(event,this)}>Sign in</Button>
+      </Col>
+    </FormGroup>
+  </Form>
     );
   }
 }
