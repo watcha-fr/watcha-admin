d41cc32c55fff67a21868a2c36ac4561d41cb0e4
Morisse <wigginsenders@gmail.com>
2018-09-03 14:44:45 +0200

    working login

--- src/App.js
+++ src/App.js
@@ -9,22 +9,61 @@ class App extends Component {
   constructor(props, context) {
   super(props, context);
 
-  this.handleChange = this.handleChange.bind(this);
-
   this.state = {
     userName:'',
-    password: ''
+    password: '',
+    token:'',
+    connected:false,
   };
 }
 
-  onConnection() {
+async onConnection() {
+    let self = this;
+    let homeserver = this.getServerName();
+    let userName = this.state.userName;
+    let password = this.state.password;
+
+    try {
+
+
+  // XHR POST to login
+  const loginRequest = await fetch( homeserver + '_matrix/client/r0/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      "initial_device_display_name": "Web setup account",
+      "user": userName,
+      "password": password,
+      "type": "m.login.password",
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  });
+  const loginData = JSON.parse(await loginRequest.text());
+  
+  self.setState({accessToken: loginData['access_token']});
+
+  console.log('login successful: ' + loginData);
+
+  return this.state.accessToken;
+
+} catch(e) {
+  console.log('error: ' + e);
+
+  return;
+}
 
 }
 
-handleChange(e) {
-  this.setState({ value: e.target.value });
+getServerName() {
+  let currentLocation = window.location.hostname;
+  console.log(currentLocation);
+  return "https://localhost:8448/"
 }
+
   render() {
+    this.getServerName();
     return (
       <Form horizontal>
     <FormGroup controlId="formHorizontalName">
@@ -41,12 +80,12 @@ handleChange(e) {
         Password
       </Col>
       <Col sm={10}>
-        <FormControl type="password" placeholder="Password" onChange={evt => this.setState({userName: evt.target.value})}/>
+        <FormControl type="password" placeholder="Password" onChange={evt => this.setState({password: evt.target.value})}/>
       </Col>
     </FormGroup>
     <FormGroup>
       <Col smOffset={2} sm={10}>
-        <Button type="submit" onClick={(event) => this.onConnection(event,this)}>Sign in</Button>
+        <Button bsStyle="primary" onClick={(event) => this.onConnection(event,this)}>Sign in</Button>
       </Col>
     </FormGroup>
   </Form>
