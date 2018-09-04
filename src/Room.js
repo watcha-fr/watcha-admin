127067c42994d5b6bd08be6c33051b47d398eceb
Morisse <wigginsenders@gmail.com>
2018-09-04 18:06:56 +0200

    create room object

---
+++ src/Room.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+export default class User extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <tr>
+        <td>{this.props.roomId}</td>
+        <td>{this.props.creator}</td>
+        <td>{this.props.admins}</td>
+        <td>{this.props.active}</td>
+        <td>{this.props.alias}</td>
+      </tr>
+    );
+  }
+
+}
