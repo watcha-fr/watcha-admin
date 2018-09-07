cf4e39273ae0b1909cbd963fca8ccca5720a4391
Morisse <wigginsenders@gmail.com>
2018-09-07 16:47:41 +0200

    make row selectable

--- src/UserTable.js
+++ src/UserTable.js
@@ -3,6 +3,7 @@ import User from './User';
 import {Table} from 'react-bootstrap';
 
 
+
 export default class UserTable extends Component {
 
   static propTypes = {
@@ -11,42 +12,37 @@ export default class UserTable extends Component {
 
   constructor(props) {
     super(props);
-
+    this.onUserSelected=this.onUserSelected.bind(this)
     this.state = {
-
+      selected:false
     };
+  }
+
+  onUserSelected(userId){
+    this.setState({ selected: userId });
+
   }
 
   render() {
     return (
-      <Table striped bordered condensed hover responsive>
-  <thead>
-    <tr>
-      <th>User Id</th>
-      <th>Creation ts</th>
-      <th>Admin</th>
-      <th>Partner</th>
-      <th>Email</th>
-    </tr>
-  </thead>
-  <tbody>
-<User userId='@joe' creationTs='12 avril' admin='false' partner='false' email='joe@mailcom'/>
-    <tr>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-    </tr>
-    <tr>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-    </tr>
-  </tbody>
-</Table>
+      <div>
+        <Table striped bordered condensed hover responsive>
+          <thead>
+            <tr>
+              <th>User Id</th>
+              <th>Creation ts</th>
+              <th>Admin</th>
+              <th>Partner</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <User userId='@joe' creationTs='12 avril 2018' admin='true' partner='false' email='joe@mailcom' onUserSelected={this.onUserSelected} selected={this.state.selected}/>
+      <User userId='@jeanne@mail.com' creationTs='14 avril 2018' admin='false' partner='true' email='jeanne@mailcom' onUserSelected={this.onUserSelected} selected={this.state.selected}/>
+          </tbody>
+        </Table>
+
+      </div>
     );
   }
 
