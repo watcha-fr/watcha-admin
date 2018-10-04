a80ba09d56c4083f1e10c76adf3a90e2d688ee59
Morisse <wigginsenders@gmail.com>
2018-10-04 10:31:27 +0200

    add a refresh button

--- src/AdminHome.js
+++ src/AdminHome.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import {Tab, Tabs} from 'react-bootstrap';
 import DataToTable from './DataToTable';
+import RefreshButton from './Buttons/RefreshButton.js';
 
 
 export default class AdminHome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      refresh: true,
+    };
+  }
+  onRefresh = () => {
+    this.setState({refresh: !this.state.refresh});
+  }
   render() {
     return (
+
       <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+        <RefreshButton onClick={this.onRefresh} />
         <Tab eventKey={1} title="Users">
-          <DataToTable tableName='user' token={this.props.token} server={this.props.server} />
+          <DataToTable tableName='user' token={this.props.token} server={this.props.server} key={this.state.refresh} />
         </Tab>
         <Tab eventKey={2} title="Rooms">
-          <DataToTable tableName='room' token={this.props.token} server={this.props.server} />
+          <DataToTable tableName='room' token={this.props.token} server={this.props.server} key={this.state.refresh} />
         </Tab>
         <Tab eventKey={3} title="Stats">
           Tab 3 content
---
+++ src/Buttons/RefreshButton.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {Glyphicon, Button} from 'react-bootstrap';
+
+export default class RefreshButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  onClick = () =>{
+    this.props.onClick();
+  }
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.onClick} bsSize="small" >
+          <Glyphicon glyph="refresh" />
+        </Button>
+      </div>
+    );
+  }
+}
