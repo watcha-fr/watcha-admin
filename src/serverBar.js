3ed57873b4f690449756965da6ec50f8b52d15b8
Morisse <wigginsenders@gmail.com>
2018-10-22 18:47:05 +0200

    add server bar

---
+++ src/serverBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { ProgressBar } from 'react-bootstrap';
+
+
+export default class ServerBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className = 'ProgressBarContainer'>
+        <div>{ this.props.label }</div>
+        <ProgressBar now={this.props.percent} label={`${this.props.percent}%`} />
+      </div>
+    );
+  }
+}
