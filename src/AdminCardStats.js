490e333649078e92b8a4d35b3b06ae5438fef692
Morisse <wigginsenders@gmail.com>
2019-03-19 10:13:37 +0100

    make column header translatable, change admin name in overview from full id to simplified id

---
+++ src/AdminCardStats.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withNamespaces } from 'react-i18next';
+
+class AdminCardStats extends Component {
+  onUserClicked= () => {
+    this.props.onUserClicked(this.props.adminName[[0]]);
+  }
+  render() {
+    return (
+      <div className="AdminName" onClick={this.onUserClicked}>
+        { this.props.simplifiedname }
+      </div>
+    );
+  }
+}
+export default withNamespaces('common')(AdminCardStats);
