b8883a193675c3b89dd1a2ff256bf242b3964c48
Morisse <wigginsenders@gmail.com>
2019-04-15 10:58:46 +0200

    use 4 space indentatation instead of 2 to match riot's indent

--- src/CollapsableRightPanel.js
+++ src/CollapsableRightPanel.js
@@ -4,77 +4,77 @@ import UserRightPanel from './UserRightPanel';
 import CreateUserRightPanel from './CreateUserRightPanel';
 
 export default class CollapsableRightPanel extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      open: true,
-    };
-  }
+        this.state = {
+            open: true,
+        };
+    }
 
 
   onClose = () => {
-    this.props.onClose();
+      this.props.onClose();
   }
 
 
   getPanel = () => {
-    let panel;
-    switch (this.props.panelType) {
+      let panel;
+      switch (this.props.panelType) {
       case 'user':
-        panel = <UserRightPanel
-          data={this.props.data}
-          onClose={this.onClose}
-          server={this.props.server}
-          token={this.props.token}
-          lang={this.props.lang}
-          refresh={this.props.refresh}
-          onTabSelected={this.props.onTabSelected}
-          refreshRightPanel={this.props.refreshRightPanel}
-          update={this.props.update}
-        />;
-        break;
+          panel = <UserRightPanel
+              data={this.props.data}
+              onClose={this.onClose}
+              server={this.props.server}
+              token={this.props.token}
+              lang={this.props.lang}
+              refresh={this.props.refresh}
+              onTabSelected={this.props.onTabSelected}
+              refreshRightPanel={this.props.refreshRightPanel}
+              update={this.props.update}
+          />;
+          break;
 
       case 'room':
-        panel = <RoomRightPanel
-          data={this.props.data}
-          onClose={this.onClose}
-          server={this.props.server}
-          token={this.props.token}
-          refresh={this.props.refresh}
-          onTabSelected={this.props.onTabSelected}
-        />;
-        break;
+          panel = <RoomRightPanel
+              data={this.props.data}
+              onClose={this.onClose}
+              server={this.props.server}
+              token={this.props.token}
+              refresh={this.props.refresh}
+              onTabSelected={this.props.onTabSelected}
+          />;
+          break;
 
       case 'createUser':
-        panel = <CreateUserRightPanel
-          data={this.props.data}
-          onClose={this.onClose}
-          server={this.props.server}
-          token={this.props.token}
-          refresh={this.props.refresh}
-          isEmailAvailable={this.props.isEmailAvailable}
-          onTabSelected={this.props.onTabSelected}
-        />;
-        break;
+          panel = <CreateUserRightPanel
+              data={this.props.data}
+              onClose={this.onClose}
+              server={this.props.server}
+              token={this.props.token}
+              refresh={this.props.refresh}
+              isEmailAvailable={this.props.isEmailAvailable}
+              onTabSelected={this.props.onTabSelected}
+          />;
+          break;
 
       default:
-        panel=<UserRightPanel
-          data={this.props.data}
-          onClose={this.onClose}
-          server={this.props.server}
-          token={this.props.token}
-          refresh={this.props.refresh}
-          lang={this.props.lang}
-        />;
-    }
-    return panel;
+          panel=<UserRightPanel
+              data={this.props.data}
+              onClose={this.onClose}
+              server={this.props.server}
+              token={this.props.token}
+              refresh={this.props.refresh}
+              lang={this.props.lang}
+          />;
+      }
+      return panel;
   }
   render() {
-    const PANEL=this.getPanel();
+      const PANEL=this.getPanel();
 
-    return (
-      <div > { PANEL } </div>
-    );
+      return (
+          <div > { PANEL } </div>
+      );
   }
 }
