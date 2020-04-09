54b3466fcf19a86f3aed3332e218e4681f95575a
c-cal <57274151+c-cal@users.noreply.github.com>
2020-04-09 23:27:18 +0200

    Watcha op261 automatic formatting and refactoring (#5)
    * refactor: format code with Prettier
    
    * refactor: use react-codemod sort-comp
    
    * refactor: use js-codemod object-shorthand
    
    * refactor: use js-codemod rm-object-assign
    
    * refactor: use js-codemod trailing-commas

--- src/CollapsableRightPanel.js
+++ src/CollapsableRightPanel.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import RoomRightPanel from './RoomRightPanel';
-import UserRightPanel from './UserRightPanel';
-import CreateUserRightPanel from './CreateUserRightPanel';
+import React, { Component } from "react";
+import RoomRightPanel from "./RoomRightPanel";
+import UserRightPanel from "./UserRightPanel";
+import CreateUserRightPanel from "./CreateUserRightPanel";
 
 export default class CollapsableRightPanel extends Component {
     constructor(props) {
@@ -12,69 +12,73 @@ export default class CollapsableRightPanel extends Component {
         };
     }
 
+    onClose = () => {
+        this.props.onClose();
+    };
 
-  onClose = () => {
-      this.props.onClose();
-  }
+    getPanel = () => {
+        let panel;
+        switch (this.props.panelType) {
+            case "user":
+                panel = (
+                    <UserRightPanel
+                        data={this.props.data}
+                        onClose={this.onClose}
+                        server={this.props.server}
+                        token={this.props.token}
+                        lang={this.props.lang}
+                        refresh={this.props.refresh}
+                        onTabSelected={this.props.onTabSelected}
+                        refreshRightPanel={this.props.refreshRightPanel}
+                        update={this.props.update}
+                    />
+                );
+                break;
 
+            case "room":
+                panel = (
+                    <RoomRightPanel
+                        data={this.props.data}
+                        onClose={this.onClose}
+                        server={this.props.server}
+                        token={this.props.token}
+                        refresh={this.props.refresh}
+                        onTabSelected={this.props.onTabSelected}
+                    />
+                );
+                break;
 
-  getPanel = () => {
-      let panel;
-      switch (this.props.panelType) {
-      case 'user':
-          panel = <UserRightPanel
-              data={this.props.data}
-              onClose={this.onClose}
-              server={this.props.server}
-              token={this.props.token}
-              lang={this.props.lang}
-              refresh={this.props.refresh}
-              onTabSelected={this.props.onTabSelected}
-              refreshRightPanel={this.props.refreshRightPanel}
-              update={this.props.update}
-          />;
-          break;
+            case "createUser":
+                panel = (
+                    <CreateUserRightPanel
+                        data={this.props.data}
+                        onClose={this.onClose}
+                        server={this.props.server}
+                        token={this.props.token}
+                        refresh={this.props.refresh}
+                        isEmailAvailable={this.props.isEmailAvailable}
+                        onTabSelected={this.props.onTabSelected}
+                    />
+                );
+                break;
 
-      case 'room':
-          panel = <RoomRightPanel
-              data={this.props.data}
-              onClose={this.onClose}
-              server={this.props.server}
-              token={this.props.token}
-              refresh={this.props.refresh}
-              onTabSelected={this.props.onTabSelected}
-          />;
-          break;
+            default:
+                panel = (
+                    <UserRightPanel
+                        data={this.props.data}
+                        onClose={this.onClose}
+                        server={this.props.server}
+                        token={this.props.token}
+                        refresh={this.props.refresh}
+                        lang={this.props.lang}
+                    />
+                );
+        }
+        return panel;
+    };
+    render() {
+        const PANEL = this.getPanel();
 
-      case 'createUser':
-          panel = <CreateUserRightPanel
-              data={this.props.data}
-              onClose={this.onClose}
-              server={this.props.server}
-              token={this.props.token}
-              refresh={this.props.refresh}
-              isEmailAvailable={this.props.isEmailAvailable}
-              onTabSelected={this.props.onTabSelected}
-          />;
-          break;
-
-      default:
-          panel=<UserRightPanel
-              data={this.props.data}
-              onClose={this.onClose}
-              server={this.props.server}
-              token={this.props.token}
-              refresh={this.props.refresh}
-              lang={this.props.lang}
-          />;
-      }
-      return panel;
-  }
-  render() {
-      const PANEL=this.getPanel();
-
-      return (
-          <div > { PANEL } </div>
-      );
-  }
+        return <div> {PANEL} </div>;
+    }
 }
