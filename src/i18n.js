e7ca6b1d47bb90dbab90b22fcbfce59044cc72f6
c-cal <github-2c7c@zebrina.fr>
2020-04-17 22:09:05 +0200

    fix: turn off debug for i18n

--- src/i18n.js
+++ src/i18n.js
@@ -40,10 +40,10 @@ i18n
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
+        debug: false,
         fallbackLng: "en",
         ns: ["common"],
         defaultNS: "common",
-        debug: true,
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
         },
