fd4f591895c0dcef9e635ec435d470e7c909704a
c-cal <github-2c7c@zebrina.fr>
2020-04-17 11:58:41 +0200

    fix: add namespace array in i18n config

--- src/i18n.js
+++ src/i18n.js
@@ -41,6 +41,7 @@ i18n
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
         fallbackLng: "en",
+        ns: ["common"],
         defaultNS: "common",
         debug: true,
         interpolation: {
