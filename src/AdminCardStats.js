fc3e2800794d18ffa6bce78763140506406a31d0
c-cal <github-2c7c@zebrina.fr>
2020-04-21 23:07:49 +0200

    fix: typo

--- src/AdminCardStats.js
+++ src/AdminCardStats.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default ({ onUserClicked, adminName, simplifiedname }) => {
-    const onClick = () => onUserClicked(adminName[[0]]);
+    const onClick = () => onUserClicked(adminName[0]);
     return (
         <div className="AdminName" {...{ onClick }}>
             {simplifiedname}
