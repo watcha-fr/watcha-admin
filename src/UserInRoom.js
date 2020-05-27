016a6d339a6972d38cc4dd5c6786727efb0862b8
c-cal <github-2c7c@zebrina.fr>
2020-05-27 23:52:11 +0200

    refactor: remove obsolete components

--- src/UserInRoom.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default ({ onUserClicked, userName, simplifiedName }) => {
-    const onClick = () => onUserClicked(userName);
-    return <span {...{ onClick }}>{simplifiedName}</span>;
-};
