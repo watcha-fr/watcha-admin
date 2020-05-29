74b5d468ee6a13afe24e6f3224a7d29f6effdd11
c-cal <github-2c7c@zebrina.fr>
2020-05-29 14:04:31 +0200

    refactor: comment unused import

--- src/UserRightPanel.js
+++ src/UserRightPanel.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faPencilAlt,
+    // faPencilAlt,
     faCheck,
     faTimes,
 } from "@fortawesome/free-solid-svg-icons";
