fb2547158700e68a97e47ee12a22b331e779925f
c-cal <github-2c7c@zebrina.fr>
2020-06-11 15:07:04 +0200

    feat: add icons in popover

--- src/UserAccountStatusHeader.js
+++ src/UserAccountStatusHeader.js
@@ -3,22 +3,26 @@ import { Trans, useTranslation } from "react-i18next";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Popover from "react-bootstrap/Popover";
 
+import "./css/UserAccountStatusHeader.scss";
 import infoCircle from "./images/info-circle.svg";
 
 export default () => {
     const { t } = useTranslation("usersTab");
 
     const popover = (
-        <Popover className="userAccountStatusHeaderPopover">
+        <Popover className="UserAccountStatusHeader_Popover">
             <Popover.Title as="h3">{t("statusHelp.title")}</Popover.Title>
             <Popover.Content>
                 <p>
+                    <span className="status active" />
                     <Trans t={t} i18nKey={"statusHelp.content.active"} />
                 </p>
                 <p>
+                    <span className="status inactive" />
                     <Trans t={t} i18nKey={"statusHelp.content.inactive"} />
                 </p>
                 <p>
+                    <span className="status invited" />
                     <Trans t={t} i18nKey={"statusHelp.content.invited"} />
                 </p>
             </Popover.Content>
