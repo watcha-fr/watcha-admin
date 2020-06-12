27ea8fd0d001f5744ab771d549de32c3a4cf3353
Kevin ICOL <kevin.icol@hotmail.fr>
2020-06-12 17:18:59 +0200

    feat: creation of PanelSection component

---
+++ src/PanelSection.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import PanelRow from "./PanelRow";
+import Table from "react-bootstrap/Table";
+
+const ns = "dashboardTab";
+
+export default ({ panelSectionName, panelSectionInformations }) => {
+    const { t } = useTranslation(ns);
+
+    const usersPerRoleSection = (
+        <tbody>
+            <PanelRow
+                label={t("usersPanel.administrators")}
+                value={`${panelSectionInformations.administrators}`}
+            />
+            <PanelRow
+                label={t("usersPanel.collaborators")}
+                value={`${panelSectionInformations.collaborators}`}
+            />
+            <PanelRow
+                label={t("usersPanel.partners")}
+                value={`${panelSectionInformations.partners}`}
+            />
+        </tbody>
+    );
+
+    const connectedUsersSection = (
+        <tbody>
+            <PanelRow
+                label={t("usersPanel.loggedUsers")}
+                value={`${panelSectionInformations.number_of_users_logged_at_least_once}`}
+            />
+            <PanelRow
+                label={t("usersPanel.monthlyUsers")}
+                value={`${panelSectionInformations.number_of_last_month_logged_users}`}
+            />
+            <PanelRow
+                label={t("usersPanel.weeklyUsers")}
+                value={`${panelSectionInformations.number_of_last_week_logged_users}`}
+            />
+        </tbody>
+    );
+
+    const otherStatisticsSection = (
+        <tbody>
+            <PanelRow
+                label={t("usersPanel.pendingInvitationUsers")}
+                value={`${panelSectionInformations.number_of_users_with_pending_invitation}`}
+            />
+        </tbody>
+    );
+
+    return (
+        <fieldset className="watcha-fieldset">
+            <legend className="watcha-legend">
+                {t(`usersPanel.${panelSectionName}`)}
+            </legend>
+            <Table>
+                {panelSectionName === "usersPerRole" && usersPerRoleSection}
+                {panelSectionName === "connectedUsers" && connectedUsersSection}
+                {panelSectionName === "otherStatistics" &&
+                    otherStatisticsSection}
+            </Table>
+        </fieldset>
+    );
+};
