8684cf8e7fc8552394da87bcc17e22893d5a18a4
Kevin ICOL <kevin.icol@hotmail.fr>
2020-06-16 16:01:05 +0200

    feat: add alt attribute to expandButton.

--- src/Buttons/ExpandButton.js
+++ src/Buttons/ExpandButton.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import ExpandLogo from "../images/expand-button.svg"
+import {useTranslation} from "react-i18next"
+
+const ns = "common"
 
 export default ({ onClick }) => {
+    const { t } = useTranslation(ns);
+
     return (
         <Button className="expandButton" {...{ onClick }}>
-            <img src={ExpandLogo} alt="expandedButton"></img>
+            <img src={ExpandLogo} alt={t("expand")}></img>
         </Button>
     );
 };
