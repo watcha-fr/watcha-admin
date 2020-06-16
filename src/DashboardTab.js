c9203184b9a4ffc9b4d82b22bd99032b4b82b1d3
Kevin ICOL <kevin.icol@hotmail.fr>
2020-06-16 15:51:09 +0200

    refactor: refactor dashboardTab to functional component.

--- src/DashboardTab.js
+++ src/DashboardTab.js
@@ -1,78 +1,65 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useGet } from "restful-react";
 import Col from "react-bootstrap/Col";
 import CardDeck from "react-bootstrap/CardDeck";
 import DelayedSpinner from "./DelayedSpinner";
 import RefreshButton from "./Buttons/RefreshButton";
 import DashboardPanel from "./DashboardPanel";
-import { MatrixClientContext } from "./contexts";
 
-class DashboardTab extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            apiAdress: "_matrix/client/r0/watcha_admin_stats",
-            data: undefined,
-            loading: true,
-        };
-    }
+export default () => {
+    const [loading, setLoading] = useState(true);
 
-    static contextType = MatrixClientContext;
+    const [dashboardInformations, setDashboardInformations] = useState([]);
 
-    getDatas = async () => {
-        const client = this.context;
-        let data;
-        try {
-            const adminStatsRequest = await fetch(
-                new URL(this.state.apiAdress, client.baseUrl),
-                {
-                    method: "GET",
-                    headers: {
-                        Authorization: "Bearer " + client.getAccessToken(),
-                    },
-                }
-            );
-            data = JSON.parse(await adminStatsRequest.text());
-        } catch (e) {
-            console.log("error: " + e);
-            return;
+    const { data, refetch } = useGet({
+        path: "watcha_admin_stats",
+        lazy: true,
+    });
+
+    const refetchRef = useRef();
+    refetchRef.current = refetch;
+
+    const intervalIdRef = useRef();
+
+    useEffect(() => {
+        refetchRef.current();
+    }, []);
+
+    useEffect(() => {
+        setDashboardInformations(data);
+        loading && setLoading(false);
+        if (intervalIdRef.current) {
+            clearInterval(intervalIdRef.current);
         }
-        this.setState({ data, loading: false });
-    };
+        intervalIdRef.current = setInterval(() => refetchRef.current(), 10000);
+    }, [data]);
 
-    componentDidMount() {
-        this.getDatas();
-    }
+    return loading || !dashboardInformations ? (
+        <DelayedSpinner />
+    ) : (
+        <div>
+            {/* <RefreshButton onClick={this.getDatas} variant="primary" /> */}
 
-    render() {
-        return this.state.loading || !this.state.data ? (
-            <DelayedSpinner />
-        ) : (
-            <div>
-                <RefreshButton onClick={this.getDatas} variant="primary" />
-
-                <CardDeck className="dashboardPanelsContainer">
-                    <Col>
-                        <DashboardPanel
-                            panelName="roomsPanel"
-                            panelInformations={this.state.data.rooms}
-                            administrateButtonTabDestination="rooms"
-                        />
-                        <DashboardPanel
-                            panelName="serverStatePanel"
-                            panelInformations={this.state.data.server}
-                        />
-                    </Col>
-                    <Col>
-                        <DashboardPanel
-                            panelName="usersPanel"
-                            panelInformations={this.state.data.users}
-                            administrateButtonTabDestination="users"
-                        />
-                    </Col>
-                </CardDeck>
-            </div>
-        );
-    }
-}
-
-export default DashboardTab;
+            <CardDeck className="dashboardTab_contentLayout">
+                <Col>
+                    <DashboardPanel
+                        panelName="roomsPanel"
+                        panelInformations={dashboardInformations.rooms}
+                        administrateButtonTabDestination="rooms"
+                    />
+                    <DashboardPanel
+                        panelName="serverStatePanel"
+                        panelInformations={dashboardInformations.server}
+                    />
+                </Col>
+                <Col>
+                    <DashboardPanel
+                        panelName="usersPanel"
+                        panelInformations={dashboardInformations.users}
+                        administrateButtonTabDestination="users"
+                    />
+                </Col>
+            </CardDeck>
+        </div>
+    );
+};
